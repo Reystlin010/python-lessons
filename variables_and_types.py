812c29d337c081a836c38c3d089235ec3b968bbc
Georgy Pestovsky <workbench010@gmail.com>
2023-02-09 23:24:14 +0300

    added variables and types

---
+++ variables_and_types.py
@@ -0,0 +1,29 @@
+variable_1 = "apple"
+print(variable_1)
+print(type(variable_1))
+# Можно изменять значение переменной в течение работы программы,
+# вместе с этим может меняться и тип данных в ней
+variable_1 = 15  
+print(variable_1)
+print(type(variable_1))
+
+red = "красный"
+Red = "зеленый"
+print(red)  # Названия переменных чувствительны к регистру
+
+message = "Hello world"  # Тип данных - строка
+print(type(message))
+
+integer_number = 10  # Тип данных - целое число
+print(type(integer_number))
+
+float_number = 5.5  # Тип данных - дробное число
+print(type(float_number))
+
+answer = True  # Тип данных - логическое значение
+print(type(answer))
+
+
+
+
+
